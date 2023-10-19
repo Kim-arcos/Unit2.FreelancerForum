add7a2e588c9a04cc8bf404291f0cf89ce79c7f3
Kim-arcos <kimarcos16@gmail.com>
2023-10-19 16:42:20 -0700

    started setup for workshop

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Is your HTML file connected to your JS file?
+Have you defined arrays for possible names and occupations?
+Have you defined an initial array of freelancers?
+Do you know how you want the freelancers' information to be displayed on the page?
+Have you written and called a function to render the initial freelancer data?
+Have you written a function to generate a new random freelancer?
+Is this function being called in an interval?
+Have you written a function to calculate the average starting price of your freelancers' array?
+When should this function be called to update the displayed message?
+*/
