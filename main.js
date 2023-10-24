c4a2fab64c32b295ccc0210a823db1a1ddc49440
Kim-arcos <kimarcos16@gmail.com>
2023-10-23 17:35:36 -0700

    followed along in class

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ Is this function being called in an interval?
 Have you written a function to calculate the average starting price of your freelancers' array?
 When should this function be called to update the displayed message?
 */
+const freeLanceWriters = [
+    {
+        name: 'Alice',
+        occupations: 'Writer',
+        startingPrice: 30
+    },
+    {
+        name: 'Bob',
+        occupations: 'Teacher',
+        startingPrice: 50
+    },
+    {
+        name: 'Carol',
+        occupation: 'Programmer',
+        startingPrice: 70
+    }
+]
+function render() {
+    const freelanceWritersEl = document.querySelector
+    const template = freeLanceWriters.map(writer => )
+    const tr= document.createElement("tr");
+    const td1 = document.createElement("td");
+    cd1.textContent = writer.name;
+    const td2 = document.createElement("td");
+    cd2.textContent = '$${writer.startingPrice}';
+    cr.append(td1, td2, td3);
+    return tr;
+}
+
+    freelanceWritersEl.replaceChildren(...template);
